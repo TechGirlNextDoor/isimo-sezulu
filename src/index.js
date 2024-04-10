69939f0b6488718421c99bbcba9a14db84ae670e
Tech Girl Next Door <87764310+TechGirlNextDoor@users.noreply.github.com>
2024-04-10 14:21:56 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
+  //searchCity("Cape Town");
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", handleSearchSubmit);
+  
+  let currentDateELement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  
+  currentDateELement.innerHTML = formatDate(currentDate);
+
+  function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInputElement.value;
+  }  
+
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+  
+    searchCity(searchInput.value);
+  }
+
+  function searchCity(city) {
+    let apiKey = "b683364f99eed30b29obcftfaa3f43bb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshData);
+  }
+
+  function refreshData(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    //let timeElement = document.querySelector(".current-date");
+    //let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+  
+    cityElement.innerHTML = response.data.city;
+   // timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+    //iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  }
+
   function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
@@ -25,52 +74,3 @@
     let formattedDay = days[day];
     return `${formattedDay} ${hours}:${minutes}`;
   }
-
-  function refreshData(response) {
-    let temperatureElement = document.querySelector(".temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#current-city");
-    let descriptionElement = document.querySelector(".description");
-    let humidityElement = document.querySelector(".humidity");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector(".current-date");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector(".icon");
-  
-    cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-  }
-  
-  function searchCity(city) {
-    let apiKey = "b683364f99eed30b29obcftfaa3f43bb";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(refreshData);
-  }
-  
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-  
-    searchCity(searchInput.value);
-  }
-  
-  function search(event) {
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInputElement.value;
-  }
-  
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", search);
-  
-  //let currentDateELement = document.querySelector("#current-date");
-  //let currentDate = new Date();
-  
-  currentDateELement.innerHTML = formatDate(currentDate);
-  searchCity("Johannesburg");
