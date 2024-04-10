3e08cbcce715801921c91df1007a6c79f6a25c78
Tech Girl Next Door <87764310+TechGirlNextDoor@users.noreply.github.com>
2024-04-10 14:51:06 +0200

    added weather icon

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
-    //iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   }
 
   function formatDate(date) {
